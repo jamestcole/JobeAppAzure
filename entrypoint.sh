906bd197c18c9454f128588307e7c9a48c49422f
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2023-11-19 15:33:10 -0500

    Override paragraph text using build argument

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# Replace <HERE> with the value of MY_VARIABLE in index.html
-sed -i 's|{REPLACE_ME}|'"$MY_VARIABLE"'|g' /usr/share/nginx/html/index.html
+# Replace <REPLACE_ME> with the value of STRING_TO_REPLACE_PARAGRAPH_TEXT in index.html
+sed -i 's|<REPLACE_ME>|'"$STRING_TO_REPLACE_PARAGRAPH_TEXT"'|g' /usr/share/nginx/html/index.html
 
 # Start nginx in the foreground
 nginx -g 'daemon off;'
