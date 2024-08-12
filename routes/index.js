8d9ceaefd2ac125b20c8922cdcb098687cafa49d
jamestcole <jamestcole@yahoo.co.uk>
2024-08-12 20:09:05 +0100

    main

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
-var express = require('express');
-var router = express.Router();
-const sql = require('../db');// Import the database connection
+const express = require('express');
+const router = express.Router();
+const { poolPromise } = require('../db'); // Import the database connection pool
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   try {
-    // Fetch data from the database
-    const result = await sql.query('SELECT * FROM Users'); // Adjust query as needed
+    const pool = await poolPromise; // Get the connection pool
+    const result = await pool.request()
+      .query('SELECT * FROM Users'); // Replace with your actual query
 
     const textToReplaceSubtitleWith = process.env.TEXT_TO_REPLACE_SUBTITLE_WITH || 'Default Subtitle Text';
     
@@ -22,3 +23,4 @@ router.get('/', async function(req, res, next) {
 });
 
 module.exports = router;
+
